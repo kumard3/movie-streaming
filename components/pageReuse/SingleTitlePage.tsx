c5d1d549c14bb5cbfebda88a05c1f30b23a699ba
kumard3 <kumardeepanshu157200@gmail.com>
2022-08-14 19:20:55 +0530

    updated the api

--- components/pageReuse/SingleTitlePage.tsx
+++ components/pageReuse/SingleTitlePage.tsx
@@ -65,7 +65,7 @@ const SingleTitlePage = ({ isTV, similarTitlesList, singleTitleDetails }: Single
         {isTV ? (
           <div className="flex h-[30rem] justify-between w-full flex-wrap">
             <iframe
-              src={`https://tmdbapi.xyz/tv-tmdb/${id}-${tvShowSeason}-${tvShowValue}`}
+              src={`https://tmdbapi.xyz/tmdb/series/?video_id=${id}&s=${tvShowSeason}&e=${tvShowValue}`}
               frameBorder="0"
               scrolling="no"
               height="100%"
@@ -78,7 +78,7 @@ const SingleTitlePage = ({ isTV, similarTitlesList, singleTitleDetails }: Single
         ) : (
           <div className="flex h-[30rem] justify-between w-full flex-wrap my-[10rem]">
             <iframe
-              src={`https://tmdbapi.xyz/tmdb-movie/${id}`}
+              src={`https://tmdbapi.xyz/tmdb/films/?video_id=${id}`}
               frameBorder="0"
               scrolling="no"
               height="100%"
